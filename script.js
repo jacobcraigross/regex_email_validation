9da2e8282c1f5a47f99cd95c127a359bb9fbea2d
Jake Ross <survivaldawn@gmail.com>
2021-04-13 09:31:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,38 +17,50 @@ function showSuccess(input) {
     formControl.className = 'form-control success';
 }
 
-function isValidEmail(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLocaleLowerCase());
+function checkPassMatch(pass_one, pass_two) {
+    if(pass_one.value !== pass_two.value) {
+        showError(pass_two, 'Passwords do not match.')
+    }
+}
+
+function checkEmail(inputItem) {
+    const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(regexEmail.test(inputItem.value.trim())) {
+        showSuccess(inputItem);
+    } else {
+        showError(inputItem, 'Email is not valid.');
+    }
+}
+
+function checkReq(inputArray) {
+    inputArray.forEach(function(inputItem) {
+        if(inputItem.value.trim() === '') {
+            showError(inputItem, `${getFieldName(inputItem)} is required`);
+        } else {
+            showSuccess(inputItem);
+        }
+    });
+}
+
+function getFieldName(inputItem) {
+    return inputItem.id.charAt(0).toUpperCase() + inputItem.id.slice(1);
+}
+
+function checkLen(inputItem, min, max) {
+    if(inputItem.value.length < min) {
+        showError(inputItem, `${getFieldName(inputItem)} must be at least ${min} characters.`);
+    } else if(inputItem.value.length > max) {
+        showError(inputItem, `${getFieldName(inputItem)} cannot be more than ${max} characters.`);
+    } else {
+        showSuccess(inputItem);
+    }
 }
 
 form.addEventListener('submit', function(e) {
-
     e.preventDefault();
-
-    if(username.value === '') {
-        showError(username, 'username you must!');
-    } else {
-        showSuccess(username);
-    }
-    
-    if(email.value === '') {
-        showError(email, 'email you must!');
-    } else if (!isValidEmail(email.value)) {
-        showError(email, 'email not valid fugger!');
-    } else {
-        showSuccess(email);
-    }
-    
-    if(password.value === '') {
-        showError(password, 'password you must!');
-    } else {
-        showSuccess(password);
-    }
-    
-    if(password_two.value === '') {
-        showError(password_two, 'password you must!');
-    } else {
-        showSuccess(password_two);
-    }
+    checkReq([username, email, password, confirm_password]);
+    checkLen(username, 6, 15);
+    checkLen(password, 8, 25);
+    checkEmail(email);
+    checkPassMatch(password, confirm_password);
 });
